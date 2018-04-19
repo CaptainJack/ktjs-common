15d116523e72dc89b2f84c4c027421b498996a3b
Alexey Chuprin <a.chuprin@capjack.ru>
2018-04-19 13:02:28 +0300

    Reconfigure build

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,11 @@
 import org.jetbrains.kotlin.gradle.tasks.Kotlin2JsCompile
+import ru.capjack.degos.publish.DegosPublishExtension
 
 group = "ru.capjack.ktjs"
 
 plugins {
 	id("kotlin2js") version "1.2.31"
-	id("ru.capjack.degos-publish") version "1.3.1-dev.3+develop.0ee4745"
+	id("ru.capjack.degos-publish") version "1.4.0"
 	id("nebula.release") version "6.0.0"
 }
 
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,17 +2,18 @@ rootProject.name = "ktjs-common"
 
 pluginManagement {
 	repositories {
-		gradlePluginPortal()
 		maven("http://artifactory.capjack.ru/public")
 	}
 	resolutionStrategy {
 		eachPlugin {
-			if (requested.id.id.startsWith("kotlin")) {
-				useModule("org.jetbrains.kotlin:kotlin-gradle-plugin:${requested.version}")
+			val id = requested.id.id
+			when {
+				id.startsWith("kotlin")            ->
+					useModule("org.jetbrains.kotlin:kotlin-gradle-plugin:${requested.version}")
+				id.startsWith("ru.capjack.degos-") ->
+					useModule("ru.capjack.degos:${id.substring(11)}:${requested.version}")
 			}
-			if (requested.id.id == "ru.capjack.degos-publish") {
-				useModule("ru.capjack.degos:degos-publish:${requested.version}")
-			}
+			
 		}
 	}
 }
