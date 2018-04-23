8c71c16fb30bcd2fcc1bf296e75472d25366e929
Alexey Chuprin <a.chuprin@capjack.ru>
2018-04-23 15:51:35 +0300

    Refactor EmptyProgress to ControllableProgress

---
+++ src/main/kotlin/ru/capjack/ktjs/common/progress/ControllableProgress.kt
@@ -0,0 +1,12 @@
+package ru.capjack.ktjs.common.progress
+
+open class ControllableProgress() : AbstractProgress() {
+	override fun calculatePercent() = 0.0
+	
+	fun makeComplete() {
+		complete()
+	}
+}
+
+fun progress(init: ControllableProgress.() -> Unit): ControllableProgress {
+	return ControllableProgress().apply(init)}
--- src/main/kotlin/ru/capjack/ktjs/common/progress/EmptyProgress.kt
+++
@@ -1,9 +0,0 @@
-package ru.capjack.ktjs.common.progress
-
-open class EmptyProgress : AbstractProgress() {
-	override fun calculatePercent() = 0.0
-	
-	fun makeComplete() {
-		complete()
-	}
-}
