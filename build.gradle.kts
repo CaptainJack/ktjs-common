6ee2a33ab8e6b8236e8b3cd70ba74f1ee83de761
Alexey Chuprin <a.chuprin@capjack.ru>
2018-05-18 14:41:40 +0300

    Update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ group = "ru.capjack.ktjs"
 
 plugins {
 	id("kotlin2js") version "1.2.41"
-	id("ru.capjack.degos.publish") version "1.6.0-dev.0.uncommitted+0f5a1d2"
+	id("ru.capjack.degos.publish") version "1.6.0"
 	id("nebula.release") version "6.0.0"
 }
 
