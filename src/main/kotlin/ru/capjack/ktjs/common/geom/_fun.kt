8506e3d762f73620804cfab881cbb2d5fbeab530
Alexey Chuprin <a.chuprin@capjack.ru>
2018-06-05 10:57:56 +0300

    Improve geom functions

--- src/main/kotlin/ru/capjack/ktjs/common/geom/_fun.kt
+++ src/main/kotlin/ru/capjack/ktjs/common/geom/_fun.kt
@@ -1,13 +1,12 @@
 package ru.capjack.ktjs.common.geom
 
-fun <T> axial(horizontal: T, vertical: T): Axial<T> {
-	return AxialImpl(horizontal, vertical)
-}
-
 fun <T> axial(both: T): Axial<T> {
 	return AxialImpl(both, both)
 }
 
+fun <T> axial(horizontal: T, vertical: T): Axial<T> {
+	return AxialImpl(horizontal, vertical)
+}
 
 fun <T> axial(supplier: (axis: Axis) -> T): Axial<T> {
 	return AxialImpl(
@@ -23,22 +22,31 @@ fun <T> axial(axis: Axis, axisValue: T, oppositeAxisValue: T): Axial<T> {
 	}
 }
 
-
-fun <T> mutableAxial(horizontal: T, vertical: T): MutableAxial<T> {
-	return MutableAxialImpl(horizontal, vertical)
-}
+//
 
 fun <T> mutableAxial(both: T): MutableAxial<T> {
 	return MutableAxialImpl(both, both)
 }
 
-fun axialInt(horizontal: Number, vertical: Number): Axial<Int> {
-	return AxialImpl(horizontal.toInt(), vertical.toInt())
+fun <T> mutableAxial(horizontal: T, vertical: T): MutableAxial<T> {
+	return MutableAxialImpl(horizontal, vertical)
 }
 
-fun axialDouble(horizontal: Number, vertical: Number): Axial<Double> {
-	return AxialImpl(horizontal.toDouble(), vertical.toDouble())
+fun <T> mutableAxial(supplier: (axis: Axis) -> T): MutableAxial<T> {
+	return MutableAxialImpl(
+		supplier(Axis.X),
+		supplier(Axis.Y)
+	)
 }
+
+fun <T> mutableAxial(axis: Axis, axisValue: T, oppositeAxisValue: T): MutableAxial<T> {
+	return when (axis) {
+		Axis.X -> MutableAxialImpl(axisValue, oppositeAxisValue)
+		Axis.Y -> MutableAxialImpl(oppositeAxisValue, axisValue)
+	}
+}
+
+//
 
 fun insets(both: Int): Insets<Int> {
 	return IntInsets(both)
@@ -52,6 +60,21 @@ fun insets(left: Int, right: Int, top: Int, bottom: Int): Insets<Int> {
 	return IntInsets(left, right, top, bottom)
 }
 
+//
+
+fun mutableInsets(both: Int): Insets<Int> {
+	return MutableIntInsets(both)
+}
+
+fun mutableInsets(horizontal: Int, vertical: Int): Insets<Int> {
+	return MutableIntInsets(horizontal, vertical)
+}
+
+fun mutableInsets(left: Int, right: Int, top: Int, bottom: Int): Insets<Int> {
+	return MutableIntInsets(left, right, top, bottom)
+}
+
+//
 
 fun rect(x: Int, y: Int, width: Int, height: Int): Rectangle<Int> {
 	return IntRectangle(x, y, width, height)
