2cd9d0c7db9bb5fff00c7ecb7e72663bd084041d
Alexey Chuprin <a.chuprin@capjack.ru>
2018-06-05 15:02:44 +0300

    Improve geom functions

--- src/main/kotlin/ru/capjack/ktjs/common/geom/MutableInsets.ext.kt
+++ src/main/kotlin/ru/capjack/ktjs/common/geom/MutableInsets.ext.kt
@@ -1,10 +1,14 @@
 package ru.capjack.ktjs.common.geom
 
+fun <T : Number> MutableInsets<T>.set(left: T, right: T, top: T, bottom: T) {
+	this.left = left
+	this.right = right
+	this.top = top
+	this.bottom = bottom
+}
+
 fun <T : Number> MutableInsets<T>.set(x: T, y: T) {
-	left = x
-	right = x
-	top = y
-	bottom = y
+	set(x, x, y, y)
 }
 
 fun <T : Number> MutableInsets<T>.set(both: T) {
