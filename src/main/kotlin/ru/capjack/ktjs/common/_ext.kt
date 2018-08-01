a43e8872708879cc222bc63490156bbb52480e6e
Alexey Chuprin <a.chuprin@capjack.ru>
2018-08-01 14:24:56 +0300

    Refactoring

--- src/main/kotlin/ru/capjack/ktjs/common/_ext.kt
+++ src/main/kotlin/ru/capjack/ktjs/common/_ext.kt
@@ -10,8 +10,16 @@ fun <T> T.replaceIf(condition: Boolean, to: T): T {
 	return if (condition) to else this
 }
 
-fun <T> T.replaceIf(condition: Boolean, to: (T) -> T): T {
+inline fun <T> T.replaceIf(condition: Boolean, to: (T) -> T): T {
 	return if (condition) to(this) else this
+}
+
+inline fun <T> T.replaceIf(condition: () -> Boolean, to: T): T {
+	return if (condition()) to else this
+}
+
+inline fun <T> T.replaceIf(condition: () -> Boolean, to: (T) -> T): T {
+	return if (condition()) to(this) else this
 }
 
 fun <T : Comparable<T>> T.replaceIfLess(value: T, to: T): T {
