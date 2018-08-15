a4d4bc6ffa1c6e54a9fc6556cf5fc054418d4ff3
Alexey Chuprin <a.chuprin@capjack.ru>
2018-08-15 15:58:52 +0300

    Improve ProgressRunner helpers

--- src/main/kotlin/ru/capjack/ktjs/common/progress/_fun.kt
+++ src/main/kotlin/ru/capjack/ktjs/common/progress/_fun.kt
@@ -14,3 +14,15 @@ inline fun progressRunner(crossinline runner: () -> Progress): ProgressRunner {
 fun progressRunner(vararg runners: () -> Progress): ProgressRunner {
 	return CompositeProgressRunner(runners.map(::progressRunner))
 }
+
+fun progressRunner(vararg runners: ProgressRunner): ProgressRunner {
+	return CompositeProgressRunner(*runners)
+}
+
+fun sequentialProgressRunner(vararg runners: ProgressRunner): ProgressRunner {
+	return SequentialProgressRunner(*runners)
+}
+
+fun sequentialProgressRunner(vararg runners: () -> Progress): ProgressRunner {
+	return SequentialProgressRunner(runners.map(::progressRunner))
+}
