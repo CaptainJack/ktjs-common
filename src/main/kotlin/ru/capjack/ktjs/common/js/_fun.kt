a7f1ea4f447b9bb7194b59ce9a4719eb30ebd8d6
Alexey Chuprin <a.chuprin@capjack.ru>
2018-08-29 08:36:38 +0300

    Improve js functions

--- src/main/kotlin/ru/capjack/ktjs/common/js/_fun.kt
+++ src/main/kotlin/ru/capjack/ktjs/common/js/_fun.kt
@@ -2,6 +2,12 @@
 
 package ru.capjack.ktjs.common.js
 
+inline fun <T> jsi(init: (T) -> Unit): T {
+	val o: T = js("{}")
+	init(o)
+	return o
+}
+
 inline fun <T> jso(init: T.() -> Unit): T {
 	val o: T = js("{}")
 	o.init()
@@ -9,16 +15,6 @@ inline fun <T> jso(init: T.() -> Unit): T {
 }
 
 inline fun <T> jso(): T {
-	return js("{}")
-}
-
-inline fun <T : Any> jst(init: T.() -> Unit): T {
-	val o: T = js("{}")
-	o.init()
-	return o
-}
-
-inline fun <T : Any> jst(): T {
 	return js("{}")
 }
 
