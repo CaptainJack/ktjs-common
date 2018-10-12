28dbdf55c1e353cdc876404cd6671b94374ba871
Alexey Chuprin <a.chuprin@capjack.ru>
2018-10-12 17:38:50 +0300

    Improve ChangeableValue

--- src/main/kotlin/ru/capjack/ktjs/common/ChangeableValue.kt
+++ src/main/kotlin/ru/capjack/ktjs/common/ChangeableValue.kt
@@ -2,4 +2,8 @@ package ru.capjack.ktjs.common
 
 interface ChangeableValue<out V> : Changeable {
 	val value: V
+	
+	fun onChange(handler: (V) -> Unit): Cancelable
+	
+	fun offChange(handler: (V) -> Unit)
 }
--- src/main/kotlin/ru/capjack/ktjs/common/MutableChangeableValueImpl.kt
+++ src/main/kotlin/ru/capjack/ktjs/common/MutableChangeableValueImpl.kt
@@ -2,6 +2,30 @@ package ru.capjack.ktjs.common
 
 import ru.capjack.ktjs.common.Delegates.observable
 
-open class MutableChangeableValueImpl<V>(value: V) : ChangeableImpl(), MutableChangeableValue<V> {
-	override var value: V by observable(value, ::dispatchChange)
+open class MutableChangeableValueImpl<V>(value: V) : MutableChangeableValue<V>, Destroyable {
+	private var handlers = ProcedureGroup()
+	
+	override var value: V by observable(value) { v ->
+		handlers.invoke(v)
+	}
+	
+	override fun onChange(handler: (V) -> Unit): Cancelable {
+		return handlers.add(handler)
+	}
+	
+	override fun onChange(handler: () -> Unit): Cancelable {
+		return handlers.add(handler)
+	}
+	
+	override fun offChange(handler: () -> Unit) {
+		handlers.remove(handler)
+	}
+	
+	override fun offChange(handler: (V) -> Unit) {
+		handlers.remove(handler)
+	}
+	
+	override fun destroy() {
+		handlers.clear()
+	}
 }
