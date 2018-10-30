0c9ef241e2e2a3e57d3504509629c66246a2cb2b
Alexey Chuprin <a.chuprin@capjack.ru>
2018-10-30 13:50:33 +0300

    Improve Changeable

--- src/main/kotlin/ru/capjack/ktjs/common/ChangeableObject.kt
+++ src/main/kotlin/ru/capjack/ktjs/common/ChangeableObject.kt
@@ -1,6 +1,6 @@
 package ru.capjack.ktjs.common
 
-class ChangeableObject<T> : AbstractChangeable<T>() {
+open class ChangeableObject<T> : AbstractChangeable<T>() {
 	public override fun introduceChange(value: T) {
 		super.introduceChange(value)
 	}
